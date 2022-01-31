6e33fb2a3e51a4f11358b5ef5a519fe878491bfc
Ali <Ali@test.com>
2022-01-31 07:51:55 +0300

    added animation when the button is clicked

--- scripts/todo.js
+++ scripts/todo.js
@@ -2,10 +2,11 @@
 
 todoButton = document.querySelectorAll(".todo-list-button");
 todoList = document.querySelectorAll(".todo-list");
+todoBorder = document.querySelectorAll(".list-border");
 
 for (let i = 0; i < todoButton.length; i++) {
   todoButton[i].addEventListener("click", function () {
     console.log("shoot");
-    todoList[i].classList.toggle("active");
+    todoBorder[i].classList.toggle("active");
   });
 }
