e7607bb557ba08cd8cf4af90313eccdfcede89d0
Ali <Ali@test.com>
2022-01-31 08:05:28 +0300

    added comments

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,12 +1,23 @@
+// Enables strict mode
+
 ("use strict");
+
+// Variables declarations
 
 todoButton = document.querySelectorAll(".todo-list-button");
 todoList = document.querySelectorAll(".todo-list");
 todoBorder = document.querySelectorAll(".list-border");
 
+// Loop to add an event listener to each classes that is selected above
+
 for (let i = 0; i < todoButton.length; i++) {
+  // Adds an Event listener for the list button
+
   todoButton[i].addEventListener("click", function () {
     console.log("shoot");
+
+    // Toggles the class when the button is clicked
+
     todoBorder[i].classList.toggle("active");
     todoButton[i].classList.toggle("active-btn");
   });
